859a137f677144de8cd3b893f2ab14cbad4d736d
Anuraag001 <anuraagbv1@gmail.com>
2023-08-15 12:21:52 +0530

    music section JS done for play_pause

--- productionfiles/js/music_playing.js
+++ productionfiles/js/music_playing.js
@@ -16,10 +16,7 @@ function update(buttonElement){
         success:function(data){
             $("#song_image").html(`<img src="${data.song_image}" height="50" width="50"  style="border-radius:5px" alt="Song Image"> `);
             $("#song_name").text(data.song_name);
-            $("#song_audio").html(`<audio id="audio-player"><source src="${data.song_audio}" type="audio/mpeg"></audio><div class="progress" role="progressbar" aria-label="Example 20px high" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100" style="height: 10px;border-radius: 5px;width:100%">
-            <div class="progress-bar" style="width: 0%"></div>
-          </div>`),
-            $('#song_controls').html(`<div><button id="play-btn"><i class="bi bi-skip-backward-fill"></i></button></div><div><button id="play-btn"><i class="bi bi-play-fill" id="change_me" onclick="icon_change()"></i></button></div><div><button id="play-btn"><i class="bi bi-skip-forward-fill"></i></button></div>`)
+            $("#song_audio").html(`<audio id="audio-player" controls><source src="${data.song_audio}" type="audio/mpeg"></audio>`),
            console.log('success',data)
         }
     });
@@ -43,11 +40,10 @@ function update_my(buttonElement){
         success:function(data){
             $("#song_image").html(`<img src="${data.song_image}" height="50" width="50"  style="border-radius:5px" alt="Song Image"> `);
             $("#song_name").text(data.song_name);
-            $("#song_audio").html(`<audio id="audio-player"><source src=" https://www.learningcontainer.com/wp-content/uploads/2020/02/Kalimba.mp3" type="audio/mpeg"></audio>
-            <div class="progress" role="progressbar" aria-label="Example 20px high" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100" style="height: 10px;border-radius: 5px;width:100%">
-              <div class="progress-bar" style="width: 0%"></div>
+            $("#song_audio").html(`<audio id="audio-player"><source src=" https://www.learningcontainer.com/wp-content/uploads/2020/02/Kalimba.mp3" type="audio/mpeg"></audio><button id="play-btn"><i class="bi bi-play-fill" id="change_me" onclick="icon_change()"></i></button>
+            <div class="progress" role="progressbar" aria-label="Example 20px high" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100" style="height: 5px;border-radius: 5px;width:50%">
+              <div class="progress-bar" style="width: 10%"></div>
             </div>`),
-            $('#song_controls').html(`<div><button id="play-btn"><i class="bi bi-skip-backward-fill"></i></button></div><div><button id="play-btn"><i class="bi bi-play-fill" id="change_me" onclick="icon_change()"></i></button></div><div><button id="play-btn"><i class="bi bi-skip-forward-fill"></i></button></div>`)
            console.log('success',data)
         }
     });
@@ -57,7 +53,7 @@ function icon_change(){
 var play_pause_toggle = document.getElementById('play-btn');
 var play_pause_btn = document.getElementById('change_me');
 var audio_play = document.getElementById('audio-player');
-var progressbar = document.getElementsByClassName('progress-bar')[0];
+var progressbar = document.getElementsByClassName('progress-bar')[0]; // Access the first element
 var progress = document.getElementsByClassName('progress')[0];
 
 let isplaying = false;
@@ -69,20 +65,20 @@ play_pause_toggle.addEventListener('click', function () {
         play_pause_btn.classList.remove('bi-play-fill');
         play_pause_btn.classList.add('bi-pause-fill');
         //console.log('pause');
-        isplaying = false;
+        isplaying = false; // Change isplaying to true when pausing
         //bi bi-pause-fill
     } else if (play_pause_btn.classList.contains('bi-pause-fill')) {
         play_pause_btn.classList.remove('bi-pause-fill');
         play_pause_btn.classList.add('bi-play-fill');
         //console.log('play');
-        isplaying = true; 
+        isplaying = true; // Change isplaying to false when playing
     }
 
     if (isplaying) {
         audio_play.pause();
         //console.log('doing');
     } else {
-        audio_play.play();
+        audio_play.play(); // Use play() method to start playing
     }
 });
 
